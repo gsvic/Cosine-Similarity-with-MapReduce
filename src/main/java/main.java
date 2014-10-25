169c8529e7f1a907d0db89b73cc62e22832729e1
Victor Giannakouris - Salalidis <victorasgs@gmail.com>
2014-10-25 03:06:17 +0300

    main

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -14,50 +14,14 @@
  * limitations under the License.
  */
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.mapreduce.Job;
 import com.gsvic.csmr.*;
 import java.io.IOException;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.mahout.math.VectorWritable;
 
 public class main {
     public static void main(String[] args) 
             throws IOException, InterruptedException, ClassNotFoundException{
-        Configuration conf = new Configuration();
-        Job job;
-        Path input, output;
-        input = new Path(args[0]);
-        output = new Path(args[1]);
-        
-        job = new Job();
-        job.setJobName("CSMR Job");
-        job.setJarByClass(main.class);
-        
-        FileInputFormat.addInputPath(job, input);
-        FileOutputFormat.setOutputPath(job, output);        
-        
-        job.setMapperClass(CSMRMapper.class);
-        job.setReducerClass(CSMRReducer.class); 
-
-        
-        job.setInputFormatClass(SequenceFileInputFormat.class);
-        
-        job.setMapOutputKeyClass(IntWritable.class);
-        job.setMapOutputValueClass(DocumentWritable.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(VectorWritable.class);
-
-
-        
-        
-        
-        System.exit( job.waitForCompletion(true) ? 1 : 0 );
+        CSMRBase.generatePairs(args[0],args[1]);
+        CSMRBase.StartCSMR();
     }
     
 }
